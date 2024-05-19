37f8aca0df476eff7196867964952f4b2a4a3519
thisiselijah <kll78460@gmail.com>
2024-05-19 23:24:20 +0800

    fix

--- Client.py
+++ Client.py
@@ -98,7 +98,7 @@ class Client:
             for element in self.speed:
                 if element == 'x1.0':
                     self.var.set(element)
-                    self.menu_1.add_radiobutton(label=element, variable=self.var, value=element, command=self.playSpeed)
+                self.menu_1.add_radiobutton(label=element, variable=self.var, value=element, command=self.playSpeed)
             self.menu_0.add_cascade(label='Speed', menu=self.menu_1)
             self.master.config(menu=self.menu_0)
         else:
